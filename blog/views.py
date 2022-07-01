d2a852296760e840167e61adcae200f7d283facb
Marcos Ornellas <129.ornellas@gmail.com>
2022-07-01 00:17:39 -0300

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
+# O ponto antes de models significa diretório atual ou aplicativo atual. 
+# Tanto views.py como models.py estão no mesmo diretório. 
+# Isto significa que podemos usar . e o nome do arquivo (sem py). 
+# Em seguida, importamos o nome do modelo (Post).
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+    
+    # o posts virou nosso queryset... como se a gente desse um nome pra ele, 
+    # ou seja, podemos criar uma variável para um comando DB?
+    # o queryset é o nome usado para a linha de comando que faz pesquisa no banco de dados? 
